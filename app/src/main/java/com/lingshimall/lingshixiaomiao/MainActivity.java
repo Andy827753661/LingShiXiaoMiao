bb89d62bb4c0ee666007989829b089636df59758
zhai <zhaiyao326@163.com>
2016-07-06 15:50:50 +0800

    radiobutton监听

--- app/src/main/java/com/lingshimall/lingshixiaomiao/MainActivity.java
+++ app/src/main/java/com/lingshimall/lingshixiaomiao/MainActivity.java
@@ -1,16 +1,52 @@
 package com.lingshimall.lingshixiaomiao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private RadioGroup mian_radiogroup;
+    private RadioButton mian_radiobutton_home, mian_radiobutton_sale, mian_radiobutton_subject, mian_radiobutton_mine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试提交
+        initView();
+        aboutRadioBUttonChecked();
 
     }
+
+    private void aboutRadioBUttonChecked() {
+
+        mian_radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.main_radiobutton_home:
+                        break;
+                    case R.id.main_radiobutton_sale:
+                        break;
+                    case R.id.main_radiobutton_subject:
+                        break;
+                    case R.id.main_radiobutton_mine:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mian_radiogroup= (RadioGroup) findViewById(R.id.main_radiogroup);
+        mian_radiobutton_home= (RadioButton) findViewById(R.id.main_radiobutton_home);
+        mian_radiobutton_sale= (RadioButton) findViewById(R.id.main_radiobutton_sale);
+        mian_radiobutton_subject= (RadioButton) findViewById(R.id.main_radiobutton_subject);
+        mian_radiobutton_mine= (RadioButton) findViewById(R.id.main_radiobutton_mine);
+    }
+
 }
