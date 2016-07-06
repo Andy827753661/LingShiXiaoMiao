98beef196ff3fe9f88f252245c4448c7fcf700f3
zhai <zhaiyao326@163.com>
2016-07-06 11:28:51 +0800

    radiogroup框架

---
+++ app/src/main/java/com/lingshimall/lingshixiaomiao/activitys/MyCatActivity.java
@@ -0,0 +1,16 @@
+package com.lingshimall.lingshixiaomiao.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lingshimall.lingshixiaomiao.R;
+
+public class MyCatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_cat);
+
+    }
+}
