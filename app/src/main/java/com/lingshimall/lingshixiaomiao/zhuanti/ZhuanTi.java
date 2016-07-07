fc341d90aeb33a9d754e5a740701e6614ff23c5c
zhai <zhaiyao326@163.com>
2016-07-07 21:05:00 +0800

    专题

--- app/src/main/java/com/lingshimall/lingshixiaomiao/zhuanti/ZhuanTi.java
+++ app/src/main/java/com/lingshimall/lingshixiaomiao/zhuanti/ZhuanTi.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ListView;
 
 import com.lingshimall.lingshixiaomiao.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by 张 波 on 2016/7/6.
@@ -15,6 +20,13 @@ import com.lingshimall.lingshixiaomiao.R;
 public class ZhuanTi extends Fragment {
 
     private View view;
+    private GridView gridView;
+    private ListView listView;
+
+    private  ArrayList<HashMap<String, Object>> listItems;
+
+    private String[] item = {"日韩", "欧美", "台湾", "韩国"};
+
     public ZhuanTi() {
 
     }
@@ -27,7 +39,11 @@ public class ZhuanTi extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_zhuanti_layout, container, false);
+        view = inflater.inflate(R.layout.fragment_zhuanti_layout, null);
+
+        gridView = (GridView) view.findViewById(R.id.zhuanti_gv_id);
+        listView = (ListView) view.findViewById(R.id.zhuanti_lv_id);
+
         return view;
     }
 
@@ -35,5 +51,27 @@ public class ZhuanTi extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        aboutGridView();
+        aboutListView();
+    }
+
+    private void aboutListView() {
+        listItems = new ArrayList<HashMap<String, Object>>();
+        //将数组信息分别存入ArrayList中
+        int len = item.length;
+        for (int i = 0; i < len; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+
+            map.put("item", item[i]);
+            listItems.add(map);
+        }
+
+        gridView.setAdapter(new ZhuantiGridAdapter(listItems,getContext()));
+
+    }
+
+    private void aboutGridView() {
+
     }
 }
