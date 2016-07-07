d1e0dd2eb576caec9f4171d08a15b86c5c65be23
zhai <zhaiyao326@163.com>
2016-07-07 17:18:51 +0800

    我的布局

---
+++ app/src/main/java/com/lingshimall/lingshixiaomiao/fragments/MyCatAboutOrderFragment.java
@@ -0,0 +1,24 @@
+package com.lingshimall.lingshixiaomiao.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lingshimall.lingshixiaomiao.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MyCatAboutOrderFragment extends Fragment {
+
+    public MyCatAboutOrderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_cat_about_order, container, false);
+    }
+}
