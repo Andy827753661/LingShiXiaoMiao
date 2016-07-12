51bd334e06aa3cb9116e59a16b07afacf105e9d9
quanquan <593099895@qq.com>
2016-07-12 11:49:27 +0800

    数据

---
+++ app/src/main/java/com/lingshimall/lingshixiaomiao/shouye/LoadUtile.java
@@ -0,0 +1,14 @@
+package com.lingshimall.lingshixiaomiao.shouye;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by quanquan on 2016/7/11.
+ */
+public class LoadUtile {
+
+
+
+
+}
